b0dff810e640afc947c2d5230f63ff1f387344c2
Matthew Olivo <saviorxtanren@gmail.com>
2017-06-22 18:06:52 -0700

    - Adding addition chat permissions needed and waiting for shortcode bug on Mixer's side to be fixed

--- Mixer.UnitTests/AuthorizationUnitTests.cs
+++ Mixer.UnitTests/AuthorizationUnitTests.cs
@@ -24,10 +24,26 @@ namespace Mixer.UnitTests
 
                 AuthorizationUnitTests.client = MixerClient.ConnectViaShortCode(clientID, new List<ClientScopeEnum>()
                 {
-                    ClientScopeEnum.chat__chat,
-                    ClientScopeEnum.chat__connect,
                     ClientScopeEnum.channel__details__self,
                     ClientScopeEnum.channel__update__self,
+
+                    //ClientScopeEnum.chat__bypass_links,
+                    //ClientScopeEnum.chat__bypass_slowchat,
+                    //ClientScopeEnum.chat__change_ban,
+                    //ClientScopeEnum.chat__change_role,
+                    ClientScopeEnum.chat__chat,
+                    //ClientScopeEnum.chat__clear_messages,
+                    ClientScopeEnum.chat__connect,
+                    //ClientScopeEnum.chat__edit_options,
+                    //ClientScopeEnum.chat__giveaway_start,
+                    //ClientScopeEnum.chat__poll_start,
+                    //ClientScopeEnum.chat__poll_vote,
+                    //ClientScopeEnum.chat__purge,
+                    //ClientScopeEnum.chat__remove_message,
+                    //ClientScopeEnum.chat__timeout,
+                    //ClientScopeEnum.chat__view_deleted,
+                    ClientScopeEnum.chat__whisper,
+
                     ClientScopeEnum.user__details__self,
                     ClientScopeEnum.user__log__self,
                     ClientScopeEnum.user__notification__self,
