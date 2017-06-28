56d16bf3e6667cbf6c7dbfac1ccceff371ca9e93
Matthew Olivo <saviorxtanren@gmail.com>
2017-06-28 00:28:45 -0700

    - Renaming solution file

--- Mixer.Base/Web/WebSocketClientBase.cs
+++ Mixer.Base/Web/WebSocketClientBase.cs
@@ -1,8 +1,6 @@
 ﻿using Mixer.Base.Model;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.WebSockets;
