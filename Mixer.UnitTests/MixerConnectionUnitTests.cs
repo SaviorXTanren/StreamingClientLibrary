b96922a1c202fd9ce946b76d27ce26f9432b4432
Matthew Olivo <saviorxtanren@gmail.com>
2017-07-11 12:22:37 -0700

    - Updating unit tests to include new OAuth redirect logic - Still need to fix Chat unit tests

--- Mixer.UnitTests/MixerConnectionUnitTests.cs
+++ Mixer.UnitTests/MixerConnectionUnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Mixer.Base;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
@@ -10,83 +11,41 @@ namespace Mixer.UnitTests
     [TestClass]
     public class MixerConnectionUnitTests : UnitTestBase
     {
-        private static MixerConnection connection;
-
-        public static MixerConnection GetMixerClientViaShortCode()
+        [TestMethod]
+        public void AuthorizeViaShortCode()
         {
-            if (MixerConnectionUnitTests.connection == null)
+            try
             {
-                string clientID = ConfigurationManager.AppSettings["ClientID"];
-                if (string.IsNullOrEmpty(clientID))
-                {
-                    Assert.Fail("ClientID value isn't set in application configuration");
-                }
-
-                MixerConnectionUnitTests.connection = MixerConnection.ConnectViaShortCode(clientID, new List<ClientScopeEnum>()
-                {
-                    ClientScopeEnum.channel__details__self,
-                    ClientScopeEnum.channel__update__self,
-
-                    //ClientScopeEnum.chat__bypass_links,
-                    //ClientScopeEnum.chat__bypass_slowchat,
-                    //ClientScopeEnum.chat__change_ban,
-                    //ClientScopeEnum.chat__change_role,
-                    ClientScopeEnum.chat__chat,
-                    //ClientScopeEnum.chat__clear_messages,
-                    ClientScopeEnum.chat__connect,
-                    //ClientScopeEnum.chat__edit_options,
-                    //ClientScopeEnum.chat__giveaway_start,
-                    //ClientScopeEnum.chat__poll_start,
-                    //ClientScopeEnum.chat__poll_vote,
-                    //ClientScopeEnum.chat__purge,
-                    //ClientScopeEnum.chat__remove_message,
-                    //ClientScopeEnum.chat__timeout,
-                    //ClientScopeEnum.chat__view_deleted,
-                    ClientScopeEnum.chat__whisper,
-
-                    ClientScopeEnum.interactive__manage__self,
-                    ClientScopeEnum.interactive__robot__self,
-
-                    ClientScopeEnum.user__details__self,
-                    ClientScopeEnum.user__log__self,
-                    ClientScopeEnum.user__notification__self,
-                    ClientScopeEnum.user__update__self,
-                },
+                MixerConnection connection = MixerConnection.ConnectViaShortCode(clientID, new List<ClientScopeEnum>() { ClientScopeEnum.chat__connect },
                 (ShortCode code) =>
                 {
                     Assert.IsNotNull(code);
                     Process.Start("https://mixer.com/oauth/shortcode?code=" + code.code);
                 }).Result;
-            }
 
-            Assert.IsNotNull(MixerConnectionUnitTests.connection);
-            return MixerConnectionUnitTests.connection;
+                Assert.IsNotNull(connection);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
         }
 
         [TestMethod]
-        public void GetAuthorizationCodeURLForOAuth()
-        {
-            string clientID = ConfigurationManager.AppSettings["ClientID"];
-            if (string.IsNullOrEmpty(clientID))
-            {
-                Assert.Fail("ClientID value isn't set in application configuration");
-            }
-
-            string url = MixerConnection.GetAuthorizationCodeURLForOAuthBrowser(clientID, new List<ClientScopeEnum>()
-            {
-                ClientScopeEnum.chat__connect,
-            }, "http://localhost").Result;
-
-            Assert.IsNotNull(url);
-        }
-
-        [TestMethod]
-        public void AuthorizeViaShortCode()
+        public void AuthorizeViaOAuthRedirect()
         {
             this.TestWrapper((MixerConnection connection) =>
             {
                 return Task.FromResult(0);
             });
+        }
+
+        [TestMethod]
+        public void GetAuthorizationCodeURLForOAuth()
+        {
+            string url = MixerConnection.GetAuthorizationCodeURLForOAuthBrowser(clientID, scopes, "http://localhost").Result;
+
+            Assert.IsNotNull(url);
         }
 
         [TestMethod]
--- Mixer.UnitTests/UnitTestBase.cs
+++ Mixer.UnitTests/UnitTestBase.cs
@@ -3,12 +3,52 @@ using Mixer.Base;
 using Mixer.Base.Model.Client;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Threading.Tasks;
 
 namespace Mixer.UnitTests
 {
     public abstract class UnitTestBase
     {
+        public static readonly string clientID = ConfigurationManager.AppSettings["ClientID"];
+        public static readonly List<ClientScopeEnum> scopes = new List<ClientScopeEnum>()
+        {
+            ClientScopeEnum.channel__details__self,
+            ClientScopeEnum.channel__update__self,
+
+            ClientScopeEnum.chat__chat,
+            ClientScopeEnum.chat__clear_messages,
+            ClientScopeEnum.chat__connect,
+            ClientScopeEnum.chat__giveaway_start,
+            ClientScopeEnum.chat__poll_start,
+            ClientScopeEnum.chat__poll_vote,
+            ClientScopeEnum.chat__purge,
+            ClientScopeEnum.chat__remove_message,
+            ClientScopeEnum.chat__timeout,
+            ClientScopeEnum.chat__whisper,
+
+            ClientScopeEnum.interactive__manage__self,
+            ClientScopeEnum.interactive__robot__self,
+
+            ClientScopeEnum.user__details__self,
+            ClientScopeEnum.user__log__self,
+            ClientScopeEnum.user__notification__self,
+            ClientScopeEnum.user__update__self,
+        };
+
+        private static MixerConnection connection;
+
+        public MixerConnection GetMixerClient()
+        {
+            if (UnitTestBase.connection == null)
+            {
+                UnitTestBase.connection = MixerConnection.ConnectViaLocalhostOAuthBrowser(clientID, scopes).Result;
+            }
+
+            Assert.IsNotNull(UnitTestBase.connection);
+            return UnitTestBase.connection;
+        }
+
         protected List<MethodPacket> methodPackets = new List<MethodPacket>();
         protected List<ReplyPacket> replyPackets = new List<ReplyPacket>();
         protected List<EventPacket> eventPackets = new List<EventPacket>();
@@ -24,7 +64,7 @@ namespace Mixer.UnitTests
         {
             try
             {
-                MixerConnection connection = MixerConnectionUnitTests.GetMixerClientViaShortCode();
+                MixerConnection connection = this.GetMixerClient();
                 function(connection).Wait();
             }
             catch (Exception ex)
