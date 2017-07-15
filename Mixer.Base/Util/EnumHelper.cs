c2f1598f992624ece9d621b90dfeb5b0e4721f2f
Matthew Olivo <saviorxtanren@gmail.com>
2017-07-15 10:13:15 -0700

    - Adding additional EnumHelper functions

--- Mixer.Base/Util/EnumHelper.cs
+++ Mixer.Base/Util/EnumHelper.cs
@@ -1,28 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Mixer.Base.Util
 {
     public static class EnumHelper
-    {
-        public static IEnumerable<string> EnumListToStringList<T>(IEnumerable<T> list)
+    {        
+        public static string GetEnumName<T>(T value)
+        {
+            MemberExpression member = value as MemberExpression;
+            if (member != null)
+            {
+                NameAttribute[] attributes = (NameAttribute[])Attribute.GetCustomAttributes(member.Member, typeof(NameAttribute), false);
+                if (attributes != null && attributes.Length > 0)
+                {
+                    return attributes[0].Name;
+                }
+            }
+
+            return Enum.GetName(typeof(T), value);
+        }
+
+        public static IEnumerable<string> GetEnumNames<T>(IEnumerable<T> list)
         {
             List<string> results = new List<string>();
             foreach (T value in list)
             {
-                results.Add(EnumHelper.EnumToString(value));
+                results.Add(EnumHelper.GetEnumName(value));
             }
             return results;
         }
-        
-        public static string EnumToString<T>(T value)
+
+        public static IEnumerable<string> GetEnumNames<T>()
         {
-            string enumName = Enum.GetName(typeof(T), value);
-            if (string.IsNullOrEmpty(enumName))
+            List<T> values = new List<T>();
+            foreach (T value in Enum.GetValues(typeof(T)))
             {
-                throw new ArgumentException("Invalid enum specified: " + value);
+                value.Equals(value);
             }
-            return enumName;
+            return EnumHelper.GetEnumNames(values);
+        }
+
+        public static T GetEnumValueFromString<T>(string str)
+        {
+            foreach (T value in Enum.GetValues(typeof(T)))
+            {
+                if (string.Equals(str, EnumHelper.GetEnumName(value)))
+                {
+                    return value;
+                }
+            }
+            return default(T);
         }
     }
 }
