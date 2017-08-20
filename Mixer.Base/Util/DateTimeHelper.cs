d5922d8864318f874d4b6e9d3e1678ebbbdf7637
Matthew Olivo <saviorxtanren@gmail.com>
2017-08-20 02:09:25 -0700

    - Updating DateTimeHelper to use .NET methods

--- Mixer.Base/Util/DateTimeHelper.cs
+++ Mixer.Base/Util/DateTimeHelper.cs
@@ -4,16 +4,8 @@ namespace Mixer.Base.Util
 {
     public static class DateTimeHelper
     {
-        private static readonly DateTimeOffset EpochDateTimeOffset = new DateTimeOffset(1970, 1, 1, 0, 0, 0, DateTimeOffset.Now.Offset);
+        public static DateTimeOffset UnixTimestampToDateTimeOffset(long milliseconds) { return DateTimeOffset.FromUnixTimeMilliseconds(milliseconds).ToOffset(DateTimeOffset.Now.Offset); }
 
-        public static DateTimeOffset UnixTimestampToDateTimeOffset(long milliseconds)
-        {
-            return EpochDateTimeOffset.AddMilliseconds(milliseconds);
-        }
-
-        public static long DateTimeOffsetToUnixTimestamp(DateTimeOffset dateTime)
-        {
-            return (long)dateTime.Subtract(EpochDateTimeOffset).TotalMilliseconds;
-        }
+        public static long DateTimeOffsetToUnixTimestamp(DateTimeOffset dateTime) { return dateTime.ToUnixTimeMilliseconds(); }
     }
 }
