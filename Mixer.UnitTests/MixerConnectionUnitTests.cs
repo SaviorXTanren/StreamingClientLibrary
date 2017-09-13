59c6079f01a9e6151bcc23e814075182a80c40e7
Matthew Olivo <saviorxtanren@gmail.com>
2017-09-13 01:29:24 -0700

    - Removing unnecessary token refreshes from unit test

--- Mixer.UnitTests/MixerConnectionUnitTests.cs
+++ Mixer.UnitTests/MixerConnectionUnitTests.cs
@@ -71,11 +71,9 @@ namespace Mixer.UnitTests
             TestWrapper(async (MixerConnection connection) =>
             {
                 MixerConnection connection2 = await MixerConnection.ConnectViaOAuthToken(connection.GetOAuthTokenCopy());
-                await connection2.RefreshOAuthToken();
                 ChannelModel channel = await connection2.Channels.GetChannel("ChannelOne");
 
                 MixerConnection connection3 = await MixerConnection.ConnectViaOAuthToken(connection2.GetOAuthTokenCopy());
-                await connection3.RefreshOAuthToken();
                 channel = await connection3.Channels.GetChannel("ChannelOne");
             });
         }
