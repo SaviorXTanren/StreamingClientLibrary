fa07420deba49cd680e4e5ddd567fa464a0ecf07
Matthew Olivo <saviorxtanren@gmail.com>
2018-01-16 18:42:01 -0800

    - Adding timestamps on to UserWithGroups object

--- Mixer.Base/Model/User/UserGroupModel.cs
+++ Mixer.Base/Model/User/UserGroupModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mixer.Base.Model.User
 {
-    public class UserGroupModel
+    public class UserGroupModel : TimeStampedModel
     {
         public uint id { get; set; }
         public string type { get; set; }
