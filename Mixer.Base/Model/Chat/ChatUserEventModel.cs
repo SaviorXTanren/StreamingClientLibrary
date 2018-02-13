2d0c46e37b08c0e0faa0cc8744ea280c0f93e178
Matthew Olivo <saviorxtanren@gmail.com>
2018-02-12 22:00:57 -0800

    OnUserUpdateOccured - Username Not Populated
    fixes #22

--- Mixer.Base/Model/Chat/ChatUserEventModel.cs
+++ Mixer.Base/Model/Chat/ChatUserEventModel.cs
@@ -6,5 +6,7 @@
         public string username { get; set; }
         public string[] roles { get; set; }
         public string[] permissions { get; set; }
+
+        public uint user { get { return id; } set { this.id = value; } }
     }
 }
