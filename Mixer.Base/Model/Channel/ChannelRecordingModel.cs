22848865fc4f4b33fa8515375ecd4a55df8695a1
Mike Hacker <mhacker@outlook.com>
2018-07-20 08:36:17 -0400

    Added Channel Recording models

---
+++ Mixer.Base/Model/Channel/ChannelRecordingModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mixer.Base.Model.Channel
+{
+    public class ChannelRecordingModel : TimeStampedModel
+    {
+        public uint id { get; set; }
+        public uint channelId { get; set; }
+        public string state { get; set; }
+        public uint viewsTotal { get; set; }
+        public DateTime expiresAt { get; set; }
+        public List<ChannelVODModel> vods { get; set; }
+        public bool? viewed { get; set; }
+        public string name { get; set; }
+        public uint typeId { get; set; }
+        public uint duration { get; set; }
+        public bool? seen { get; set; }
+    }
+}
