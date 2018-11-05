9de21daf95f7f55e8d5e5b60ec41dbe122e64341
Matthew Olivo <saviorxtanren@gmail.com>
2018-11-04 21:38:10 -0800

    - Adding comments for Validator class

--- Mixer.Base/Util/Validator.cs
+++ Mixer.Base/Util/Validator.cs
@@ -4,8 +4,16 @@ using System.Linq;
 
 namespace Mixer.Base.Util
 {
+    /// <summary>
+    /// Variable validation helper class
+    /// </summary>
     public static class Validator
     {
+        /// <summary>
+        /// Validates whether the specified string has a non-null and non-empty value. Throws an ArgumentException if it does not.
+        /// </summary>
+        /// <param name="value">The string to check</param>
+        /// <param name="name">The name of the string</param>
         public static void ValidateString(string value, string name)
         {
             if (string.IsNullOrEmpty(value))
@@ -14,6 +22,11 @@ namespace Mixer.Base.Util
             }
         }
 
+        /// <summary>
+        /// Validates whether the specified variable is not null. Throws an ArgumentException if it is.
+        /// </summary>
+        /// <param name="value">The variable to check</param>
+        /// <param name="name">The name of the variable</param>
         public static void ValidateVariable(object value, string name)
         {
             if (value == null)
@@ -22,6 +35,11 @@ namespace Mixer.Base.Util
             }
         }
 
+        /// <summary>
+        /// Validates whether the specified list has is not null and empty. Throws an ArgumentException if it is.
+        /// </summary>
+        /// <param name="value">The list to check</param>
+        /// <param name="name">The name of the list</param>
         public static void ValidateList<T>(IEnumerable<T> value, string name)
         {
             Validator.ValidateVariable(value, name);
