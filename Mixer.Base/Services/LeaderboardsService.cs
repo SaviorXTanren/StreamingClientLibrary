3430fd11debdeba682717dd83862831840f8c25c
Shane DeSeranno <tyren@syntheticstream.com>
2019-01-16 14:39:05 -0800

    Add limit parameter to leaderboard APIs

--- Mixer.Base/Services/LeaderboardsService.cs
+++ Mixer.Base/Services/LeaderboardsService.cs
@@ -22,80 +22,88 @@ namespace Mixer.Base.Services
         /// Gets the weekly sparks leaderboard for the specified channel.
         /// </summary>
         /// <param name="channel">The channel to get the leaderboard for</param>
+        /// <param name="limit">The number of users to limit the results to.  Max: 100, Default: 10</param>
         /// <returns>The weekly sparks leaderboard for the channel specified</returns>
-        public async Task<IEnumerable<SparksLeaderboardModel>> GetWeeklySparksLeaderboard(ChannelModel channel)
+        public async Task<IEnumerable<SparksLeaderboardModel>> GetWeeklySparksLeaderboard(ChannelModel channel, int limit = 10)
         {
-            return await this.GetAsync<IEnumerable<SparksLeaderboardModel>>("leaderboards/sparks-weekly/channels/" + channel.id.ToString());
+            return await this.GetAsync<IEnumerable<SparksLeaderboardModel>>("leaderboards/sparks-weekly/channels/" + channel.id.ToString() + "?limit=" + limit.ToString());
         }
 
         /// <summary>
         /// Gets the monthly sparks leaderboard for the specified channel.
         /// </summary>
         /// <param name="channel">The channel to get the leaderboard for</param>
+        /// <param name="limit">The number of users to limit the results to.  Max: 100, Default: 10</param>
         /// <returns>The monthly sparks leaderboard for the channel specified</returns>
-        public async Task<IEnumerable<SparksLeaderboardModel>> GetMonthlySparksLeaderboard(ChannelModel channel)
+        public async Task<IEnumerable<SparksLeaderboardModel>> GetMonthlySparksLeaderboard(ChannelModel channel, int limit = 10)
         {
-            return await this.GetAsync<IEnumerable<SparksLeaderboardModel>>("leaderboards/sparks-monthly/channels/" + channel.id.ToString());
+            return await this.GetAsync<IEnumerable<SparksLeaderboardModel>>("leaderboards/sparks-monthly/channels/" + channel.id.ToString() + "?limit=" + limit.ToString());
         }
 
         /// <summary>
         /// Gets the yearly sparks leaderboard for the specified channel.
         /// </summary>
         /// <param name="channel">The channel to get the leaderboard for</param>
+        /// <param name="limit">The number of users to limit the results to.  Max: 100, Default: 10</param>
         /// <returns>The yearly sparks leaderboard for the channel specified</returns>
-        public async Task<IEnumerable<SparksLeaderboardModel>> GetYearlySparksLeaderboard(ChannelModel channel)
+        public async Task<IEnumerable<SparksLeaderboardModel>> GetYearlySparksLeaderboard(ChannelModel channel, int limit = 10)
         {
-            return await this.GetAsync<IEnumerable<SparksLeaderboardModel>>("leaderboards/sparks-yearly/channels/" + channel.id.ToString());
+            return await this.GetAsync<IEnumerable<SparksLeaderboardModel>>("leaderboards/sparks-yearly/channels/" + channel.id.ToString() + "?limit=" + limit.ToString());
         }
 
         /// <summary>
         /// Gets the all-time sparks leaderboard for the specified channel.
         /// </summary>
         /// <param name="channel">The channel to get the leaderboard for</param>
+        /// <param name="limit">The number of users to limit the results to.  Max: 100, Default: 10</param>
         /// <returns>The all-time sparks leaderboard for the channel specified</returns>
-        public async Task<IEnumerable<SparksLeaderboardModel>> GetAllTimeSparksLeaderboard(ChannelModel channel)
+        public async Task<IEnumerable<SparksLeaderboardModel>> GetAllTimeSparksLeaderboard(ChannelModel channel, int limit = 10)
         {
-            return await this.GetAsync<IEnumerable<SparksLeaderboardModel>>("leaderboards/sparks-alltime/channels/" + channel.id.ToString());
+            return await this.GetAsync<IEnumerable<SparksLeaderboardModel>>("leaderboards/sparks-alltime/channels/" + channel.id.ToString() + "?limit=" + limit.ToString());
         }
 
         /// <summary>
         /// Gets the weekly embers leaderboard for the specified channel.
         /// </summary>
         /// <param name="channel">The channel to get the leaderboard for</param>
+        /// <param name="limit">The number of users to limit the results to.  Max: 100, Default: 10</param>
         /// <returns>The weekly embers leaderboard for the channel specified</returns>
-        public async Task<IEnumerable<EmbersLeaderboardModel>> GetWeeklyEmbersLeaderboard(ChannelModel channel)
+        public async Task<IEnumerable<EmbersLeaderboardModel>> GetWeeklyEmbersLeaderboard(ChannelModel channel, int limit = 10)
         {
-            return await this.GetAsync<IEnumerable<EmbersLeaderboardModel>>("leaderboards/embers-weekly/channels/" + channel.id.ToString());
+            return await this.GetAsync<IEnumerable<EmbersLeaderboardModel>>("leaderboards/embers-weekly/channels/" + channel.id.ToString() + "?limit=" + limit.ToString());
         }
 
         /// <summary>
         /// Gets the monthly embers leaderboard for the specified channel.
         /// </summary>
         /// <param name="channel">The channel to get the leaderboard for</param>
+        /// <param name="limit">The number of users to limit the results to.  Max: 100, Default: 10</param>
         /// <returns>The monthly embers leaderboard for the channel specified</returns>
-        public async Task<IEnumerable<EmbersLeaderboardModel>> GetMonthlyEmbersLeaderboard(ChannelModel channel)
+        public async Task<IEnumerable<EmbersLeaderboardModel>> GetMonthlyEmbersLeaderboard(ChannelModel channel, int limit = 10)
         {
-            return await this.GetAsync<IEnumerable<EmbersLeaderboardModel>>("leaderboards/embers-monthly/channels/" + channel.id.ToString());
+            return await this.GetAsync<IEnumerable<EmbersLeaderboardModel>>("leaderboards/embers-monthly/channels/" + channel.id.ToString() + "?limit=" + limit.ToString());
         }
 
         /// <summary>
         /// Gets the yearly embers leaderboard for the specified channel.
         /// </summary>
         /// <param name="channel">The channel to get the leaderboard for</param>
+        /// <param name="limit">The number of users to limit the results to.  Max: 100, Default: 10</param>
         /// <returns>The yearly embers leaderboard for the channel specified</returns>
-        public async Task<IEnumerable<EmbersLeaderboardModel>> GetYearlyEmbersLeaderboard(ChannelModel channel)
+        public async Task<IEnumerable<EmbersLeaderboardModel>> GetYearlyEmbersLeaderboard(ChannelModel channel, int limit = 10)
         {
-            return await this.GetAsync<IEnumerable<EmbersLeaderboardModel>>("leaderboards/embers-yearly/channels/" + channel.id.ToString());
+            return await this.GetAsync<IEnumerable<EmbersLeaderboardModel>>("leaderboards/embers-yearly/channels/" + channel.id.ToString() + "?limit=" + limit.ToString());
         }
 
         /// <summary>
         /// Gets the all-time embers leaderboard for the specified channel.
         /// </summary>
         /// <param name="channel">The channel to get the leaderboard for</param>
+        /// <param name="limit">The number of users to limit the results to.  Max: 100, Default: 10</param>
         /// <returns>The all-time embers leaderboard for the channel specified</returns>
-        public async Task<IEnumerable<EmbersLeaderboardModel>> GetAllTimeEmbersLeaderboard(ChannelModel channel)
+        public async Task<IEnumerable<EmbersLeaderboardModel>> GetAllTimeEmbersLeaderboard(ChannelModel channel, int limit = 10)
         {
-            return await this.GetAsync<IEnumerable<EmbersLeaderboardModel>>("leaderboards/embers-alltime/channels/" + channel.id.ToString());
+            return await this.GetAsync<IEnumerable<EmbersLeaderboardModel>>("leaderboards/embers-alltime/channels/" + channel.id.ToString() + "?limit=" + limit.ToString());
         }
     }
 }
