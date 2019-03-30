354abafee166a5130a1ba19fd896210a7561c532
Matthew Olivo <saviorxtanren@gmail.com>
2019-03-30 16:09:29 -0700

    - Adding acquire date time to short code

--- Mixer.Base/Model/OAuth/OAuthShortCodeModel.cs
+++ Mixer.Base/Model/OAuth/OAuthShortCodeModel.cs
@@ -1,9 +1,20 @@
-﻿namespace Mixer.Base.Model.OAuth
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Mixer.Base.Model.OAuth
 {
     public class OAuthShortCodeModel
     {
         public string code { get; set; }
         public string handle { get; set; }
         public uint expires_in { get; set; }
+
+        [DataMember]
+        public DateTimeOffset AcquiredDateTime { get; set; }
+
+        public OAuthShortCodeModel()
+        {
+            this.AcquiredDateTime = DateTimeOffset.Now;
+        }
     }
 }
