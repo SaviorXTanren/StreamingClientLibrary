7a3386dfc4160d0efdff724f4979122ef787918e
Shane DeSeranno <tyren@syntheticstream.com>
2019-12-30 15:14:56 -0800

    Increase constellation timeout from 5 seconds to 30 seconds.
    Mixer's server seems to have issuessometimes and this causes the connection to fail.  Increasing the timeout gives us more time to wait for their service.

--- Mixer/Mixer.Base/Clients/ConstellationClient.cs
+++ Mixer/Mixer.Base/Clients/ConstellationClient.cs
@@ -306,7 +306,7 @@ namespace Mixer.Base.Clients
 
             await base.Connect(endpoint);
 
-            await this.WaitForSuccess(() => { return this.Connected; });
+            await this.WaitForSuccess(() => { return this.Connected; }, 30);
 
             this.OnEventOccurred -= ConstellationClient_HelloMethodHandler;
 
