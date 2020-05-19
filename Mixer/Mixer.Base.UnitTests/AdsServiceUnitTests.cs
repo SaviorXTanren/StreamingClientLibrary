5cf203cfa531677fedc9e2e9a84f9ed014c4292d
Matthew Olivo <saviorxtanren@gmail.com>
2020-05-18 20:46:52 -0700

    - Updating test library nuget packages - Adding AdsServiceUnitTests for Mixer

---
+++ Mixer/Mixer.Base.UnitTests/AdsServiceUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mixer.Base.Model.User;
+
+namespace Mixer.Base.UnitTests
+{
+    [TestClass]
+    public class AdsServiceUnitTests : UnitTestBase
+    {
+        [TestMethod]
+        public void GetCurrentBroadcast()
+        {
+            TestWrapper(async (MixerConnection connection) =>
+            {
+                PrivatePopulatedUserModel user = await connection.Users.GetCurrentUser();
+
+                Assert.IsNotNull(user);
+                Assert.IsTrue(user.id > 0);
+
+                bool success = await connection.Ads.RunAd(user.channel);
+
+                Assert.IsTrue(success);
+            });
+        }
+    }
+}
