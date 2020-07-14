d5c9ec1d12ebb656d8c804e33be0c70868080b40
Matthew Olivo <saviorxtanren@gmail.com>
2020-07-13 20:07:11 -0700

    - Adding Twitch.Extensions.Base project & sample API project

---
+++ Samples/Twitch.ExtensionSample.API/Controllers/SampleController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using Twitch.Extensions.Base.Models;
+using Twitch.Extensions.Base.Services;
+
+namespace Twitch.ExtensionSample.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        public string clientID = "";
+        public string clientSecret = "";
+        public string ownerID = "";
+
+        [HttpGet("test")]
+        public async Task<ActionResult<string>> Test()
+        {
+            if (!this.Request.Headers.TryGetValue("Authorization", out StringValues authHeader))
+            {
+                return this.Unauthorized("Missing Authorization header");
+            }
+
+            string bearerToken = authHeader.ToString();
+            if (!string.IsNullOrEmpty(bearerToken))
+            {
+                bearerToken = bearerToken.Replace("Bearer ", "");
+            }
+
+            string result = TwitchAuthenticationService.ValidateAuthenticationToken(bearerToken, clientSecret, out TwitchJWTTokenPayloadModel payload);
+            if (!string.IsNullOrEmpty(result))
+            {
+                return this.Unauthorized(result);
+            }
+
+            JObject data = new JObject();
+            data["hello"] = "world";
+            await TwitchBroadcastService.SendBroadcast(clientID, clientSecret, ownerID, payload.channel_id, data);
+
+            return this.Ok("Broadcast Sent");
+        }
+    }
+}
---
+++ Twitch/Twitch.Extensions.Base/Models/BroadcastBodyModel.cs
@@ -0,0 +1,37 @@
+﻿using StreamingClient.Base.Util;
+using System.Runtime.Serialization;
+
+namespace Twitch.Extensions.Base.Models
+{
+    /// <summary>
+    /// The body of a request to be broadcasted out
+    /// </summary>
+    [DataContract]
+    public class BroadcastBodyModel
+    {
+        /// <summary>
+        /// The content type of the body
+        /// </summary>
+        [DataMember]
+        public string content_type { get; set; } = "application/json";
+        /// <summary>
+        /// The target of the message.
+        /// </summary>
+        [DataMember]
+        public string[] targets { get; set; } = new string[] { "broadcast" };
+        /// <summary>
+        /// The serialized message.
+        /// </summary>
+        [DataMember]
+        public string message { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of the BroadcastBodyModel class.
+        /// </summary>
+        /// <param name="data">The data to include in the broadcast</param>
+        public BroadcastBodyModel(object data)
+        {
+            this.message = JSONSerializerHelper.SerializeToString(data);
+        }
+    }
+}
