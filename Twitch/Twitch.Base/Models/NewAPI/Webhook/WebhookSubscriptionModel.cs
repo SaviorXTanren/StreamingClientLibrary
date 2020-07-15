5f91c38e3f5eee9f2b7c3899c0a14bccfc4d3a07
Michael Hacker <mike@mikehacker.net>
2020-07-15 14:44:17 -0400

    Added Webhook service

---
+++ Twitch/Twitch.Base/Models/NewAPI/Webhook/WebhookSubscriptionModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Twitch.Base.Models.NewAPI.Webhooks
+{
+    /// <summary>
+    /// Information about Webhook Subscription
+    /// </summary>
+    public class WebhookSubscriptionModel
+    {
+        /// <summary>
+        /// The topic used in the initial subscription.
+        /// </summary>
+        public string topic { get; set; }
+
+        /// <summary>
+        /// The callback provided for this subscription.
+        /// </summary>
+        public string callback { get; set; }
+
+        /// <summary>
+        /// Date and time when this subscription expires. Encoded as RFC3339. The timezone is always UTC (“Z”).
+        /// </summary>
+        public string expires_at { get; set; }
+    }
+}
