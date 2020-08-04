7a8c830b4d41f981f1952e54c614e193b578369b
Matthew Olivo <saviorxtanren@gmail.com>
2020-08-04 16:27:32 -0700

    - Adding non-function Task-based methods for SemaphoreSlim.WaitAndRelease()

--- StreamingClient.Base/Util/SemaphoreSlimExtensions.cs
+++ StreamingClient.Base/Util/SemaphoreSlimExtensions.cs
@@ -10,6 +10,43 @@ namespace StreamingClient.Base.Util
     public static class SemaphoreSlimExtensions
     {
         /// <summary>
+        /// Awaits on a semaphore, awaits the specified task, &amp; then releases the semaphore.
+        /// </summary>
+        /// <param name="semaphore">The semaphore to await on</param>
+        /// <param name="task">The task to await</param>
+        /// <returns>An awaitable task</returns>
+        public static async Task WaitAndRelease(this SemaphoreSlim semaphore, Task task)
+        {
+            try
+            {
+                await semaphore.WaitAsync();
+
+                await task;
+            }
+            catch (Exception ex) { Logger.Log(ex); }
+            finally { semaphore.Release(); }
+        }
+
+        /// <summary>
+        /// Awaits on a semaphore, awaits the specified task, &amp; then releases the semaphore.
+        /// </summary>
+        /// <param name="semaphore">The semaphore to await on</param>
+        /// <param name="task">The task to await</param>
+        /// <returns>An awaitable task of type T</returns>
+        public static async Task<T> WaitAndRelease<T>(this SemaphoreSlim semaphore, Task<T> task)
+        {
+            try
+            {
+                await semaphore.WaitAsync();
+
+                return await task;
+            }
+            catch (Exception ex) { Logger.Log(ex); }
+            finally { semaphore.Release(); }
+            return default(T);
+        }
+
+        /// <summary>
         /// Awaits on a semaphore, runs the specified function, &amp; then releases the semaphore.
         /// </summary>
         /// <param name="semaphore">The semaphore to await on</param>
