f3c38d715d72e7ddd2b445b47a05cd4b2a4fadaf
Matthew Olivo <saviorxtanren@gmail.com>
2021-07-02 03:45:11 -0700

    - Adding Trovo unit tests

---
+++ Trovo/Trovo.Base.UnitTests/UsersServiceUnitTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trovo.Base.Models.Users;
+
+namespace Trovo.Base.UnitTests
+{
+    [TestClass]
+    public class UsersServiceUnitTests : UnitTestBase
+    {
+        [TestMethod]
+        public void GetCurrentUser()
+        {
+            TestWrapper(async (TrovoConnection connection) =>
+            {
+                PrivateUserModel user = await connection.Users.GetCurrentUser();
+
+                Assert.IsNotNull(user);
+                Assert.IsTrue(!string.IsNullOrEmpty(user.userId));
+                Assert.IsTrue(!string.IsNullOrEmpty(user.userName));
+            });
+        }
+
+        [TestMethod]
+        public void GetUser()
+        {
+            TestWrapper(async (TrovoConnection connection) =>
+            {
+                PrivateUserModel privateUser = await connection.Users.GetCurrentUser();
+
+                Assert.IsNotNull(privateUser);
+                Assert.IsTrue(!string.IsNullOrEmpty(privateUser.userId));
+                Assert.IsTrue(!string.IsNullOrEmpty(privateUser.userName));
+
+                UserModel user = await connection.Users.GetUser(privateUser.userName);
+
+                Assert.IsNotNull(user);
+                Assert.IsTrue(!string.IsNullOrEmpty(user.user_id));
+                Assert.IsTrue(!string.IsNullOrEmpty(user.username));
+            });
+        }
+    }
+}
